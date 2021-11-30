aee78bfeee49720b8e49157124bb49eb8506eb13
voxom <voxom13@gmail.com>
2021-11-30 15:40:01 -0600

    fixed issue: Switch -> Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
+
+
+function NoMatch() {
+  const location = useLocation();
+
+  return (
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   );
 }
 
